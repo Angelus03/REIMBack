98e9815ede75528e8aa3b13e3006a08b38b5e106
I.S.C. Angel Javier Puc Yamá <angelpuc08@gmail.com>
2020-01-23 17:02:24 -0600

    Termino creacion de cadena falta obtener numero de referencia

--- public/ftp.js
+++ public/ftp.js
@@ -1,30 +1,31 @@
 var EasyFtp = require("easy-ftp");
 var ftp = new EasyFtp();
-
-// var config = {
-//     host: "ftp://148.245.62.85",
-//     type: "ftp",
-//     port: "21",
-//     username: "MIERYTERAN",
-//     password: "pd06lfCJ"
-// };
-
-const config = {
-    host: "127.0.0.1",
-    type: "FTP",
-    port: "21",
-    username: "javi",
-    password: "javi08"
-};
+var fs = require('fs');
+var envJSON = require("../config/env.variables.json");
 
 
-exports.UploadFile = function UploadFile(ruta, nombreArchivo) {
+var config = envJSON["development"]["configFTP_MSC"];
 
+exports.UploadFile = function UploadFile(ruta, nombreArchivo, eliminarDespues) {
     ftp.connect(config);
     // console.log('FTP CONNECTED:' + ftp.client.isConnect)
 
-    ftp.upload(ruta + nombreArchivo, "/Test_MYT/" + nombreArchivo, function(err){});   
-
+    ftp.upload(ruta + nombreArchivo, "/Test_MYT/" + nombreArchivo, function (err) {
+        if (fs.existsSync(ruta + nombreArchivo)) {
+            if (err) {
+                console.log(err)
+            } else {
+                if (eliminarDespues) {
+                    fs.unlink(ruta + nombreArchivo, (err) => {
+                        if (err) {
+                            console.error(err)
+                            // return
+                        }
+                    })
+                }
+            }
+        }
+    });
     //ftp.close();
 }
 
@@ -33,8 +34,7 @@ exports.DeleteFile = function DeleteFile(nombreArchivo) {
     ftp.connect(config);
     // console.log('FTP CONNECTED:' + ftp.client.isConnect)
 
-    ftp.rm("/Test_MYT/" + nombreArchivo, function(err){});   
+    ftp.rm("/Test_MYT/" + nombreArchivo, function (err) { });
 
     //ftp.close();
 }
-
